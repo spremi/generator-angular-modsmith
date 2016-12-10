b90fd512382be10b1768a33ea8aa0f94d8ec0931
Sanjeev Premi <spremi@ymail.com>
2016-12-11 01:46:52 +0530

    gen: Add prompts
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- generators/app/index.js
+++ generators/app/index.js
@@ -23,12 +23,105 @@ module.exports = yeoman.Base.extend({
       'Welcome to the dandy ' + chalk.red('generator-angular-modsmith') + ' generator!'
     ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    var prompts = [
+      {
+        type: 'input',
+        name: 'argName',
+        message: chalk.yellow('Package name :'),
+        default: this.arg || this.dir
+      },
+      {
+        type: 'input',
+        name: 'argDesc',
+        message: chalk.yellow('Package description:'),
+        default: 'TODO: Add package description.'
+      },
+      {
+        type: 'input',
+        name: 'argVersion',
+        message: chalk.yellow('Package version:'),
+        default: '0.0.1'
+      },
+      {
+        type: 'input',
+        name: 'argRepo',
+        message: chalk.yellow('Package Repository:')
+      },
+      {
+        type: 'list',
+        name: 'argLicense',
+        choices : [
+          {
+            name  : 'Apache License 2.0',
+            value : 'Apache-2.0',
+            short : 'Apache-2.0'
+          },
+          {
+            name  : 'BSD 2-clause "Simplified" License',
+            value : 'BSD-2-Clause',
+            short : 'BSD-2-Clause'
+          },
+          {
+            name  : 'BSD 3-clause "New" or "Revised" License',
+            value : 'BSD-3-Clause',
+            short : 'BSD-3-Clause'
+          },
+          {
+            name  : 'GNU General Public License v2.0 only',
+            value : 'GPL-2.0',
+            short : 'GPL-2.0'
+          },
+          {
+            name  : 'GNU General Public License v3.0 only',
+            value : 'GPL-3.0',
+            short : 'GPL-3.0'
+          },
+          {
+            name  : 'GNU Lesser General Public License v2.1 only',
+            value : 'LGPL-2.1',
+            short : 'LGPL-2.1'
+          },
+          {
+            name  : 'GNU Lesser General Public License v3.0 only',
+            value : 'LGPL-3.0',
+            short : 'LGPL-3.0'
+          },
+          {
+            name  : 'MIT License',
+            value : 'MIT',
+            short : 'MIT'
+          },
+          {
+            name  : 'Other License',
+            value : 'Other',
+            short : 'Other'
+          }
+        ],
+        message: chalk.yellow('Select applicable license:')
+      },
+      {
+        type: 'input',
+        name: 'argKeywords',
+        message: chalk.yellow('Package keywords [comma-separated]:')
+      },
+      {
+        type: 'input',
+        name: 'argAuthorName',
+        message: chalk.yellow('Author\'s name:'),
+        default: this.user.git.name
+      },
+      {
+        type: 'input',
+        name: 'argAuthorEmail',
+        message: chalk.yellow('Author\'s email:'),
+        default: this.user.git.email
+      },
+      {
+        type: 'input',
+        name: 'argAuthorHome',
+        message: chalk.yellow('Author\'s homepage:')
+      }
+    ];
 
     return this.prompt(prompts).then(function (props) {
       // To access props later use this.props.someAnswer;
