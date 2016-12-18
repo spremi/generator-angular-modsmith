b1a900eb3466d99e51ffe5943cf9858bcabba693
Sanjeev Premi <spremi@ymail.com>
2016-12-18 23:06:08 +0530

    subgen: service: Add unit test cases
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- test/service.js
+++ test/service.js
@@ -6,13 +6,40 @@ var helpers = require('yeoman-test');
 describe('generator-angular-modsmith:service', function () {
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/service'))
-      .withPrompts({someAnswer: true})
+      .withLocalConfig({
+        pkg: {
+          name: {
+            orig: 'test-mod',
+            slug: 'test-mod',
+            camel: 'testMod'
+          },
+          desc: 'Test Module',
+          version: '0.0.1',
+          repo: '',
+          license: 'Apache-2.0',
+          keywords: [
+            'key', 'word'
+          ]
+        },
+        author: {
+          name: 'Some One',
+          email: 'some@one.two',
+          url: ''
+        }
+      })
+      .withPrompts({argName: 'delta-phi'})
       .toPromise();
   });
 
-  it('creates files', function () {
+  it('Creates source file', function () {
     assert.file([
-      'dummyfile.txt'
+      'src/services/deltaPhi/deltaPhi.service.js'
+    ]);
+  });
+
+  it('Creates spec file', function () {
+    assert.file([
+      'src/services/deltaPhi/deltaPhi.service.spec.js'
     ]);
   });
 });
