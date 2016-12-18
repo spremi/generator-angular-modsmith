a11fbb07641283d237fd23685414a7845d596223
Sanjeev Premi <spremi@ymail.com>
2016-12-18 21:26:48 +0530

    subgen: directive: Add unit test cases
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- test/directive.js
+++ test/directive.js
@@ -6,13 +6,52 @@ var helpers = require('yeoman-test');
 describe('generator-angular-modsmith:directive', function () {
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/directive'))
-      .withPrompts({someAnswer: true})
+      .withLocalConfig({
+        pkg: {
+          name: {
+            orig: 'test-mod',
+            slug: 'test-mod',
+            camel: 'testMod'
+          },
+          desc: 'Test Module',
+          version: '0.0.1',
+          repo: '',
+          license: 'Apache-2.0',
+          keywords: [
+            'key', 'word'
+          ]
+        },
+        author: {
+          name: 'Some One',
+          email: 'some@one.two',
+          url: ''
+        }
+      })
+      .withPrompts({argName: 'alpha-beta'})
       .toPromise();
   });
 
-  it('creates files', function () {
+  it('Creates source file', function () {
     assert.file([
-      'dummyfile.txt'
+      'src/directives/alphaBeta/alphaBeta.directive.js'
+    ]);
+  });
+
+  it('Creates external template', function () {
+    assert.file([
+      'src/directives/alphaBeta/alphaBeta.html'
+    ]);
+  });
+
+  it('Creates stylus file', function () {
+    assert.file([
+      'src/directives/alphaBeta/alphaBeta.styl'
+    ]);
+  });
+
+  it('Creates spec file', function () {
+    assert.file([
+      'src/directives/alphaBeta/alphaBeta.directive.spec.js'
     ]);
   });
 });
