54d839fbb1c8e6b1ab38daa172e656254cb58f8b
Sanjeev Premi <spremi@ymail.com>
2017-04-15 11:53:01 +0530

    tpl: gulp: Fix globs passed to 'eslint'
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- generators/app/templates/_gulpfile.js
+++ generators/app/templates/_gulpfile.js
@@ -8,6 +8,26 @@ var gulp = require('gulp');
 
 var excludeGitignore = require('gulp-exclude-gitignore');
 
+//
+// Path to common directories
+//
+var self = {
+  src: 'src',
+  dst: 'dist',
+  tmp: '.tmp'
+};
+
+//
+// Options to various tasks
+//
+var options = {
+  eslint: {
+    glob: [
+      self.src + '/**/*.js',
+      'test/main.js'
+    ]
+  }
+};
 
 //
 // Static 'lint' checking
@@ -15,10 +35,7 @@ var excludeGitignore = require('gulp-exclude-gitignore');
 var eslint = require('gulp-eslint');
 
 gulp.task('eslint', function () {
-  return gulp
-    .src([
-      '**/*.js'
-    ])
+  return gulp.src(options.eslint.glob)
     .pipe(excludeGitignore())
     .pipe(eslint())
     .pipe(eslint.format())
