3faf848872a9e4235ad563c7d5a7b78ee0ec2d51
Sanjeev Premi <spremi@ymail.com>
2017-04-22 23:06:41 +0530

    tpl: Add template for 'index.js'
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

---
+++ generators/app/templates/_index.js
@@ -0,0 +1,13 @@
+//
+// <%= pkg.name.slug %>
+//
+// (c) <%= (new Date).getFullYear() %> <%= author.name %> (<%= author.email %>)
+//
+// SPDX-License-Identifier: <%= pkg.license %>
+//                          (http://spdx.org/licenses/<%= pkg.license %>.html)
+//
+
+
+require('./dist/<%= pkg.name.slug %>.min.js');
+
+module.exports = '<%= pkg.name.slug %>';
