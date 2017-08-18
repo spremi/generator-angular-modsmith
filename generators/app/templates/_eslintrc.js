a90ef8bcf4248697d14722afd3ce772bdf357833
Sanjeev Premi <spremi@ymail.com>
2017-08-18 22:34:14 +0530

    tpl: eslint: Use 'eslint-plugin-angular'
    Allows us to be consistent with community.
    These rules are turned off:
      - angular/filename
          To continue using the filenames created in the generator.
    
      - angular/no-service-method
          To allow use of sub-generator for service, if developers
          really want to implement a service.
    
      - angular/function-type
          Allow developer to make a choice on using named/ anonymous
          functions.
    
    These rules may be configured by users in future releases...
    
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- generators/app/templates/_eslintrc.js
+++ generators/app/templates/_eslintrc.js
@@ -8,131 +8,14 @@ module.exports = {
     jasmine: true
   },
 
-  extends: 'eslint:recommended',
+  extends: 'plugin:angular/johnpapa',
 
   rules: {
-    //
-    // Possible errors
-    //
-    'no-console': 'off',
-    'no-debugger': 'off',
-
-    'no-inner-declarations': ['error', 'both'],
-    'no-unexpected-multiline': 'error',
-
-    //
-    // Best practices
-    //
-    curly: 'error',
-    'dot-notation': 'error',
-    eqeqeq: ['error', 'smart'],
-    'guard-for-in': 'error',
-    'no-caller': 'error',
-    'no-eval': 'error',
-    'no-extend-native': 'error',
-    'no-labels': 'error',
-    'no-lone-blocks': 'error',
-    'no-loop-func': 'error',
-    'no-multi-str': 'error',
-    'no-native-reassign': 'error',
-    'no-new-func': 'error',
-    'no-redeclare': 'error',
-    'no-with': 'error',
-    'wrap-iife': ['error', 'inside'],
-
-    //
-    // Strict mode
-    //
-    strict: 'error',
-
-    //
-    // Variables
-    //
-    'no-shadow': 'error',
-    'no-shadow-restricted-names': 'error',
-    'no-use-before-define': 'error',
-
-    //
-    // NodeJS & CommonJS
-    //
-    'global-require': 'off',
-
-    //
-    // Stylistic issues
-    //
-    'array-bracket-spacing': ['error', 'never'],
-    'block-spacing': ['error', 'always'],
-    'brace-style': ['error',
-      '1tbs',
-      {
-        allowSingleLine: true
-      }
-    ],
-    camelcase: ['error',
-      {
-        properties: 'always'
-      }
-    ],
-    'comma-spacing': ['error',
-      {
-        before: false,
-        after: true
-      }
-    ],
-    'comma-style': ['error', 'last'],
-    'computed-property-spacing': ['error', 'never'],
-    'eol-last': 'error',
-    indent: ['error',
-      2,
-      {
-        SwitchCase: 1
-      }
-    ],
-    'key-spacing': 'error',
-    'keyword-spacing': 'error',
-    'new-cap': 'error',
-    'new-parens': 'error',
-    'newline-after-var': ['error', 'always'],
-    'no-array-constructor': 'error',
-    'no-multiple-empty-lines': 'off',
-    'no-spaced-func': 'error',
-    'no-trailing-spaces': 'error',
-    'object-curly-spacing': ['error', 'always'],
-    quotes: ['error', 'single'],
-    semi: ['error', 'always'],
-    'semi-spacing': ['error',
-      {
-        before: false,
-        after: true
-      }
-    ],
-    'space-before-blocks': 'error',
-    'space-before-function-paren': ['error',
-      {
-        anonymous: 'always',
-        named: 'never'
-      }
-    ],
-    'space-in-parens': ['error', 'never'],
-    'space-infix-ops': 'error',
-    'space-unary-ops': ['warn',
-      {
-        words: true,
-        nonwords: false
-      }
-    ],
-    'spaced-comment': ['error', 'always'],
-
-    //
-    // ECMAScript 6
-    //
-    'arrow-spacing': 'error',
-
-    //
-    // AngularJS
-    //
-    'no-invalid-this': 'off'
+    'angular/file-name': 'off',
+    'angular/no-service-method': 'off',
+    'angular/function-type': 'off'
   },
+
   globals: {
     angular: true,
     define: true,
