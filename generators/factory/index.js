6656fa7863442dce5cc6a76781ad8eaa266d05ee
Sanjeev Premi <spremi@ymail.com>
2017-08-20 22:17:35 +0530

    subgen: factory: Define 'props' object
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- generators/factory/index.js
+++ generators/factory/index.js
@@ -40,8 +40,26 @@ module.exports = yeoman.Base.extend({
     ];
 
     return this.prompt(prompts).then(function (props) {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
+      //
+      // Read package configuration.
+      //
+      var pkgconf = this.config.getAll();
+
+      //
+      // Add package configuration to factory options
+      //
+      this.props = {
+        pkg     : JSON.parse(JSON.stringify(pkgconf.pkg)),
+        author  : JSON.parse(JSON.stringify(pkgconf.author)),
+        fct : {
+          name  : {
+            orig    : props.argName,
+            slug    : to.slug(props.argName),
+            camel   : to.camel(props.argName)
+          },
+          desc      : props.argDesc
+        }
+      };
     }.bind(this));
   },
 
