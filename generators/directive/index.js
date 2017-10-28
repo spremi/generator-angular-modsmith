2b236292b4a55d332b3bc33c3a5134b008ee2838
Sanjeev Premi <spremi@ymail.com>
2017-10-28 21:29:25 +0530

    subgen: directive: Commit generated sources
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- generators/directive/index.js
+++ generators/directive/index.js
@@ -188,10 +188,32 @@ module.exports = class extends Generator {
     }
   }
 
+  /**
+   * Private function to add sources to git repo and commit
+   */
+  _gitCommit(dst) {
+    const sgit = require('simple-git')(this.destinationRoot());
+
+    var msg = 'chore: Add directive \'' + this.props.dtv.name.slug + '\'\n' +
+              '\n' +
+              'Created by generator-angular-modsmith v' +
+              this.rootGeneratorVersion() + '.\n';
+
+    sgit.add(dst).commit(msg);
+
+    this.log(chalk.white.bold('\nCommitted to repo.\n'));
+  }
+
   writing() {
     var dstDir = path.join('src', 'directives', this.props.dtv.name.camel);
 
     this._createDir(dstDir);
     this._copySourceFiles(dstDir);
+  }
+
+  end() {
+    var dstDir = path.join('src', 'directives', this.props.dtv.name.camel);
+
+    this._gitCommit(dstDir);
   }
 };
