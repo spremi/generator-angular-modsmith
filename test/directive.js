55dc6e0fd17badaca8f4c9e584df1bf1d178ec25
Sanjeev Premi <spremi@ymail.com>
2017-11-25 00:38:30 +0530

    directive: Add test cases
    Signed-off-by: Sanjeev Premi <spremi@ymail.com>

--- test/directive.js
+++ test/directive.js
@@ -72,3 +72,69 @@ describe('generator-angular-modsmith:directive', () => {
     ]);
   });
 });
+
+describe('generator-angular-modsmith:directive [2]', () => {
+  before(() => {
+    return helpers.run(path.join(__dirname, '../generators/directive'))
+      .withLocalConfig({
+        pkg: {
+          name: {
+            orig: 'test-mod',
+            slug: 'test-mod',
+            camel: 'testMod'
+          },
+          desc: 'Test Module',
+          version: '0.0.1',
+          repo: '',
+          license: 'Apache-2.0',
+          keywords: [
+            'key', 'word'
+          ]
+        },
+        author: {
+          name: 'Some One',
+          email: 'some@one.two',
+          url: ''
+        }
+      })
+      .withPrompts({
+        argName: 'mu-pi',
+        argExtTpl: false,
+        argStyle: false,
+        argLinkFn: false
+      })
+      .toPromise();
+  });
+
+  it('Creates directive file', () => {
+    assert.file([
+      'src/directives/muPi/muPi.directive.js'
+    ]);
+  });
+
+  it('Creates inline template', () => {
+    assert.fileContent(
+      'src/directives/muPi/muPi.directive.js',
+      'template: \'<div>Directive: muPi</div>\''
+    );
+  });
+
+  it('Doesn\'t create external template', () => {
+    assert.noFile([
+      'src/directives/muPi/muPi.html'
+    ]);
+  });
+
+  it('Doesn\'t create stylus file', () => {
+    assert.noFile([
+      'src/directives/muPi/muPi.styl'
+    ]);
+  });
+
+  it('Doesn\'t set transclude', () => {
+    assert.noFileContent(
+      'src/directives/muPi/muPi.directive.js',
+      'transclude: true'
+    );
+  });
+});
